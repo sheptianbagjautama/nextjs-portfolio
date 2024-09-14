0a13688793d891ed5f9e24b1cdce672297506406
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-09-14 18:23:39 +0700

    fix text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${sora.variable} font-Sora flex flex-col bg-gray-50 text-gray-50 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 antialiased`}
+        className={`${sora.variable} font-Sora flex flex-col bg-gray-50 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 antialiased`}
       >
         {/* Gradient Background Color */}
         <div className="bg-[#fbe2e3] absolute top-[-6rem] flex-1 -z-[10] right-[11rem] h-[21.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68-75rem] dark:bg-[#946263]"></div>
