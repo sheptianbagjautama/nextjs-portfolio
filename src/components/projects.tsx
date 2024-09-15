da51b1143034a279eb2822f4e86e004c20cc666e
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-09-15 17:06:29 +0700

    create projects component

---
+++ src/components/projects.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useSectionInView } from "@/lib/useInView";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project-card";
+
+const Projects = () => {
+  const { ref } = useSectionInView("#projects", 0.1);
+  return (
+    <section id="projects" ref={ref} className="scroll-mt-28 mb-28">
+      <SectionHeading>My Projects</SectionHeading>
+
+      <div>
+        {projectsData.map((project, index) => (
+          <Project {...project} key={index} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
