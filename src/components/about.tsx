8ebbd1da7f05507089f806a1047461196ec2087b
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-09-16 16:12:29 +0700

    Develop about component

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -12,7 +12,7 @@ const About = () => {
   return (
     <motion.section
       ref={ref}
-      className="max-w-[45rem] text-center mt-32 leading-8 mb-28 sm:mb-40 scroll-mt-28"
+      className="max-w-[45rem] text-justify mt-32 leading-8 mb-28 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
@@ -29,11 +29,12 @@ const About = () => {
           <SectionHeading>About Me </SectionHeading>
         </Fade>
 
-        <div className="grid md:grid-cols-2 lg:text-start">
+        <div className="grid md:grid-cols-2 lg:text-justify">
+          {/* <div className="flex flex-row lg:text-justify"> */}
           <div className="flex-1">
             <div className="text-lg mt-12 xl:mt-3">
               <div className="flex justify-start flex-col">
-                <Fade
+                {/* <Fade
                   direction="up"
                   delay={400}
                   cascade
@@ -41,7 +42,7 @@ const About = () => {
                   triggerOnce={true}
                 >
                   <h3 className="font-bold mt-6">Our mission</h3>
-                </Fade>
+                </Fade> */}
 
                 <Fade
                   direction="up"
@@ -50,14 +51,29 @@ const About = () => {
                   damping={1e-1}
                   triggerOnce={true}
                 >
-                  <p className="mt-2 leading-relaxed text-sm text-gray-700 dark:text-white/70">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s
+                  <p className="mt-2 leading-relaxed text-sm sm:text-lg text-gray-700 dark:text-white/70">
+                    You can call me Tian , I am someone who{" "}
+                    <span className="font-bold">enjoys programming</span>. I buy
+                    courses and eBooks from various sources, one of them being
+                    Udemy, though I usually learn for free from YouTube.{" "}
+                    <span className="font-bold">
+                      I’m always eager to learn new things.
+                    </span>{" "}
+                    Currently, I’ve been working at PT. Nextvibes Mitra Global
+                    for the
+                    <span className="font-bold">
+                      {" "}
+                      past 5 years as a Full Stack Developer
+                    </span>
+                    , and{" "}
+                    <span className="font-bold">
+                      I’m looking for a full-time position as a Software
+                      Developer.
+                    </span>
                   </p>
                 </Fade>
 
-                <Fade
+                {/* <Fade
                   direction="up"
                   delay={800}
                   cascade
@@ -65,7 +81,7 @@ const About = () => {
                   triggerOnce={true}
                 >
                   <h3 className="font-bold mt-6">Our Vission</h3>
-                </Fade>
+                </Fade> */}
 
                 <Fade
                   direction="up"
@@ -74,10 +90,11 @@ const About = () => {
                   damping={1e-1}
                   triggerOnce={true}
                 >
-                  <p className="mt-2 leading-relaxed text-sm text-gray-700 dark:text-white/70">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s
+                  <p className="mt-2 leading-relaxed text-sm sm:text-lg text-gray-700 dark:text-white/70">
+                    When I’m not coding, I enjoy watching movies. Every day, I
+                    dedicate time to{" "}
+                    <span className="font-bold">learning new things</span>,
+                    especially in the field of programming.
                   </p>
                 </Fade>
               </div>
@@ -86,23 +103,25 @@ const About = () => {
 
           {/* Right Image */}
 
-          <Fade
-            direction="right"
-            delay={800}
-            cascade
-            damping={1e-1}
-            triggerOnce={true}
-          >
-            <Image
-              src="/tian2.png"
-              alt="About Me"
-              width={600}
-              height={600}
-              quality={100}
-              priority={true}
-              className=" mt-8 object-cover"
-            />{" "}
-          </Fade>
+          <div>
+            <Fade
+              direction="right"
+              delay={800}
+              cascade
+              damping={1e-1}
+              triggerOnce={true}
+            >
+              <Image
+                src="/tian2.png"
+                alt="About Me"
+                width={600}
+                height={600}
+                quality={100}
+                priority={true}
+                className="mt-8 object-cover"
+              />{" "}
+            </Fade>
+          </div>
         </div>
       </div>
     </motion.section>
