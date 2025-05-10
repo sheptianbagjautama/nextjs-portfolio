e6a1995db3ae06242bd4f07525b8db9cdcfb06c8
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2025-05-10 21:17:59 +0700

    change link CV file

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -91,7 +91,7 @@ const Intro = () => {
         </Link>
 
         <a
-          href="/cv.pdf"
+          href="/cv_pdf"
           download
           // target="_blank"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 dark:bg-white/10 active:scale-105 transition dark:text-white/60"
